e6ac8a13151d0564df09bf66a6060e3a75dfb15c
Gheorghita Alexandru <55880052+sandu12312@users.noreply.github.com>
2025-04-18 22:18:46 +0300

    file structure and packdge.json update

---
+++ coffeshare/app/(auth)/forgot_password.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const forgot_password = () => {
+  return (
+    <View>
+      <Text>forgot_password</Text>
+    </View>
+  );
+};
+
+export default forgot_password;
+
+const styles = StyleSheet.create({});
